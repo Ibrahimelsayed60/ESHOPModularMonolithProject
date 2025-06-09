8e79968ac20c5c36c40dcc8e10e8fb5f0e3c8161
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-06-09 08:51:58 +0300

    add interfaces for command and query for cqrs

---
+++ src/Modules/Catalog/Catalog/Features/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Features.CreateProduct
+{
+
+    public record CreateProductCommand(String Name, List<string> Category, string Description, string ImageFile, decimal Price) : IRequest<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
