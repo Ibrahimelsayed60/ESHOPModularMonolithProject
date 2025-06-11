6f082997b39e274f174cee35a9db45a3880d4e3c
Ibrahimelsayed60 <hemasayed600@gmail.com>
2025-06-11 05:31:23 +0300

    implemented endpoits: - Create Product - Update Product - Delete Product - GetProducts - GetProductById

---
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,41 @@
+﻿using Carter;
+using Catalog.Products.Dtos;
+using Mapster;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Products.Features.CreateProduct
+{
+    public record CreateProductRequest(ProductDto ProductDto);
+
+    public record CreateProductResponse(Guid Id);
+
+    public class CreateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<CreateProductResponse>();
+
+                return Results.Created($"/products/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
+        }
+    }
+}
